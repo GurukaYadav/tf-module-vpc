586fe6f24b121701948810798b0d3e621ec51e1e
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-25 12:25:08 +0530

    updating tf-module-vpc

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "var.VPC_CIDR"
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
-    Name = "roboshop-vpc"
+    Name = "${var.PROJECT }-${var.ENV}-vpc"
   }
 }
